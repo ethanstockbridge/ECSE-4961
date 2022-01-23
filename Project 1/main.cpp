29bd066992ddfa3d15f3e9093c5a3fa233f477dd
Ethan Stockbridge <ethanstockbridge@gmail.com>
2022-01-23 14:28:01 -0500

    Update main.cpp
    Adding int SIMD matrix multiplication function

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -135,8 +135,7 @@ void matrix_multiplication(Matrix<T> A, Matrix<T> B, Matrix<T>& C)
 }
 
 //test function for intrinsics matrix multiplication
-template <typename T>
-void SIMD_matrix_multiplication(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C)
+void SIMD_matrix_multiplication(float_matrix& A, float_matrix& B, float_matrix& C)
 {
     int i,j,k;
     if( (A.getCols() != B.getRows()) || 
@@ -148,10 +147,10 @@ void SIMD_matrix_multiplication(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C)
     }
 
     //create a new matrix to store inverted B in
-    T** B_inv = new T*[B.getCols()];
+    float** B_inv = new float*[B.getCols()];
     for (i=0;i<B.getRows();i++)
     {
-        B_inv[i] = new T[B.getRows()];
+        B_inv[i] = new float[B.getRows()];
     }
     //invert values of B
     for (i=0;i<B.getRows();i++)
@@ -184,6 +183,55 @@ void SIMD_matrix_multiplication(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C)
     }
 }
 
+
+void SIMD_matrix_multiplication(int_matrix& A, int_matrix& B, int_matrix& C)
+{
+    int i,j,k;
+    if( (A.getCols() != B.getRows()) || 
+        (C.getCols() != B.getCols()) ||
+        (C.getRows() != A.getRows()) )
+    {
+        cerr<<"Error, Matrix dimensions do not match"<<endl;
+        return;
+    }
+
+    //create a new matrix to store inverted B in
+    int** B_inv = new int*[B.getCols()];
+    for (i=0;i<B.getRows();i++)
+    {
+        B_inv[i] = new int[B.getRows()];
+    }
+    //invert values of B
+    for (i=0;i<B.getRows();i++)
+    {
+        for(j=0;j<B.getCols();j++)
+        {
+            B_inv[j][i] = B[i][j];
+        }
+    }
+    for (i = 0; i < A.getRows(); i++)
+    {
+        for (j = 0; j < B.getRows(); j++)
+        {
+            //multiply accumulate flattened column with flattened row
+            __m128i num1, num2, num3, num4;
+
+            num4= _mm_setzero_si128();  //sets sum to zero
+
+            for(k=0; k<A.getCols(); k+=4)
+            {
+                num1 = _mm_load_si128((__m128i*)(A[i]+k));            // load: num1 = a[3]  a[2]  a[1]  a[0]
+                num2 = _mm_load_si128((__m128i*)(B_inv[j]+k));        // load: num2 = b[3]   b[2]   b[1]  b[0]
+                num3 = _mm_mullo_epi32(num1, num2);                   // multiply, store low bits: num3 = a[3]*b[3]  a[2]*b[2]  a[1]*b[1]  a[0]*b[0]
+                num3 = _mm_hadd_epi32(num3, num3);                    // horizontal addition: num3 = [a[3]*b[3] + a[2]*b[2] , a[1]*b[1] + a[0]*b[0] , a[3]*b[3] + a[2]*b[2] , a[1]*b[1] + a[0]*b[0]]
+                num4 = _mm_add_epi32(num4, num3);                     // performs vertical addition
+            }
+            num4= _mm_hadd_epi32(num4, num4); //horizontally adds into num4
+            C[i][j] = _mm_extract_epi32(num4, 0); //sum from above is in num4[0], save to matrix
+        }
+    }
+}
+
 //main function
 int main()
 {
@@ -191,25 +239,25 @@ int main()
 
     unsigned int size = 3;
 
-    float_matrix first(size,size);
+    int_matrix first(size,size);
     first.randomize();
     cout<<"Matrix A:"<<endl;
     first.print();
 
-    float_matrix second(size,size);
+    int_matrix second(size,size);
     second.randomize();
     cout<<"Matrix B:"<<endl;
     second.print();
 
     cout<<"A x B ="<<endl;
-    float_matrix result(size,size);
+    int_matrix result(size,size);
     auto start1 = high_resolution_clock::now();
     matrix_multiplication(first, second, result);
     auto stop1 = high_resolution_clock::now();
     result.print();
 
     cout<<"A x B ="<<endl;
-    float_matrix result2(size,size);
+    int_matrix result2(size,size);
     auto start2 = high_resolution_clock::now();
     SIMD_matrix_multiplication(first, second, result2);
     auto stop2 = high_resolution_clock::now();
